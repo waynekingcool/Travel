16d55f8e756a7f75b01e4002de04f94d239603af
wanbing <271629099@qq.com>
2020-02-04 10:59:31 +0800

    111

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <home-header></home-header>
+        <div>Home</div>
+    </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+    name:"Home",
+    components:{
+        HomeHeader
+    }
+}
+</script>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>This is Header</div>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
