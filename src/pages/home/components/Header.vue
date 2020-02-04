ac2eff945ee3d0e307d42607a9653fa35547414a
wanbing <271629099@qq.com>
2020-02-04 15:42:28 +0800

    head

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">内容</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe743;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe600;</span>&nbsp;搜索内容
+        </div>
+        <div class="header-right">
+            <span class="iconfont arrow-icon">南昌&#xe64a;</span>
+        </div>
     </div>
 </template>
 
@@ -14,14 +20,20 @@ export default {
 
 // scoped表示该样式为局部样式不会影响其他
 <style lang="stylus" scoped>
+// @表示src文件夹,~表示在css样式中引入css
+// styles可以在bulid/webpack.base.conf.js中配置路径
+@import '~styles/varibles.styl'
     .header
         display flex
         line-height .86rem
-        background #00bcd4
+        background $bgColor
         color #ffffff
         .header-left
             width .64rem
             float left
+            .back-icon
+                text-align center
+                font-size .4rem
         .header-input
             flex 1
             background #ffffff
@@ -35,5 +47,7 @@ export default {
             width 1.24rem
             float right
             text-align center
+            .arrow-icon
+                font-size .24rem
         
 </style>
