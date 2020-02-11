327d749965b49b556ce4b3f68089269306f46804
wanbing <271629099@qq.com>
2020-02-11 14:39:25 +0800

    111

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
+        <city-header></city-header>
         城市
     </div>
 </template>
 
 <script>
+import CityHeader from './components/Header'
 export default {
-    name:"City"
+    name:"City",
+    components:{
+        CityHeader
+    }
 }
 </script>
 
---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to="/">
+            <div class="iconfont back-icon">&#xe743;</div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"CityHeader"
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+    position relative
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #ffffff
+    background $bgColor
+    font-size .32rem
+    .back-icon
+        position absolute
+        top 0
+        left 0
+        text-align center
+        font-size .4rem
+        width .64rem
+        color #ffffff
+</style>
