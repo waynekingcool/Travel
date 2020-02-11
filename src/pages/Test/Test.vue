c5ef5cfdb93541b1712fdc9739f14545d46e139b
wanbing <271629099@qq.com>
2020-02-11 14:53:59 +0800

    111

--- src/pages/Test/Test.vue
+++ src/pages/Test/Test.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="grid">
-        
+        <div class="iconfont back-icon">&#xe743;</div>
     </div>
 </template>
 
@@ -11,5 +11,17 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+.grid
+    overflow hidden
+    height .86rem
+    line-height .86rem
+    text-align center
+    color #ffffff
+    background #cccccc
+    font-size .32rem
+    .back-icon
+        text-align center
+        font-size .4rem
+        width .64rem
+        color #ffffff
 </style>
